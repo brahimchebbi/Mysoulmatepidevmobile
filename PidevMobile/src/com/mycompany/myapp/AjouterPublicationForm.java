ba28abc32d17a1540a8bc0ee328ceafdb49cb11f
brahimchebbi <brahim.chebbi@esprit.tn>
2018-05-07 01:17:55 +0200

    controle de saisie fait

--- PidevMobile/src/com/mycompany/myapp/AjouterPublicationForm.java
+++ PidevMobile/src/com/mycompany/myapp/AjouterPublicationForm.java
@@ -14,6 +14,7 @@ import com.codename1.notifications.LocalNotification;
 import com.codename1.ui.Command;
 import com.codename1.ui.Component;
 import com.codename1.ui.Container;
+import com.codename1.ui.Dialog;
 import com.codename1.ui.Display;
 import com.codename1.ui.EncodedImage;
 import com.codename1.ui.FontImage;
@@ -134,88 +135,19 @@ altitude.setHidden(true);
        
         
         this.addComponent(BorderLayout.CENTER, center);
-//        
-//         Container hi = new Container(new BoxLayout(BoxLayout.Y_AXIS));
-//        hi.setLayout(new BorderLayout());
-//        final MapContainer cnt = new MapContainer(HTML_API_KEY);
-//
-//        Button btnMoveCamera = new Button("Move Camera");
-//        btnMoveCamera.addActionListener(e->{
-//            cnt.setCameraPosition(new Coord(-33.867, 151.206));
-//        });
-//        Style s = new Style();
-//        s.setFgColor(0xff0000);
-//        s.setBgTransparency(0);
-//        FontImage markerImg = FontImage.createMaterial(FontImage.MATERIAL_PLACE, s, Display.getInstance().convertToPixels(3));
-//
-//        Button btnAddMarker = new Button("Add Marker");
-//        btnAddMarker.addActionListener(e->{
-//
-//            cnt.setCameraPosition(new Coord(41.889, -87.622));
-//            cnt.addMarker(
-//                    EncodedImage.createFromImage(markerImg, false),
-//                    cnt.getCameraPosition(),
-//                    "Hi marker",
-//                    "Optional long description",
-//                     evt -> {
-//                             ToastBar.showMessage("You clicked the marker", FontImage.MATERIAL_PLACE);
-//                     }
-//            );
-//
-//        });
-//
-//        Button btnAddPath = new Button("Add Path");
-//        btnAddPath.addActionListener(e->{
-//
-//            cnt.addPath(
-//                    cnt.getCameraPosition(),
-//                    new Coord(-33.866, 151.195), // Sydney
-//                    new Coord(-18.142, 178.431),  // Fiji
-//                    new Coord(21.291, -157.821),  // Hawaii
-//                    new Coord(37.423, -122.091)  // Mountain View
-//            );
-//        });
-//
-//        Button btnClearAll = new Button("Clear All");
-//        btnClearAll.addActionListener(e->{
-//            cnt.clearMapLayers();
-//        });
-//
-//        cnt.addTapListener(e->{
-//            TextField enterName = new TextField();
-//            Container wrapper = BoxLayout.encloseY(new Label("Name:"), enterName);
-//            InteractionDialog dlg = new InteractionDialog("Add Marker");
-//            dlg.getContentPane().add(wrapper);
-//            enterName.setDoneListener(e2->{
-//                String txt = enterName.getText();
-//                cnt.addMarker(
-//                        EncodedImage.createFromImage(markerImg, false),
-//                        cnt.getCoordAtPosition(e.getX(), e.getY()),
-//                        enterName.getText(),
-//                        "",
-//                        e3->{
-//                                ToastBar.showMessage("You clicked "+txt, FontImage.MATERIAL_PLACE);
-//                        }
-//                );
-//                dlg.dispose();
-//            });
-//            dlg.showPopupDialog(new Rectangle(e.getX(), e.getY(), 10, 10));
-//            enterName.startEditingAsync();
-//        });
-//
-//        Container root = LayeredLayout.encloseIn(
-//                BorderLayout.center(cnt),
-//                BorderLayout.south(
-//                        FlowLayout.encloseBottom(btnMoveCamera, btnAddMarker, btnAddPath, btnClearAll)
-//                )
-//        );
-//        this.addComponent(BorderLayout.CENTER, hi);
+
         Button AjouterB = new Button("Ajouter Publication");
 AjouterB.setUIID("AjouterButton");
         AjouterB.setTextPosition(Component.LEFT);
         
         this.addComponent(BorderLayout.SOUTH, AjouterB);
          AjouterB.addActionListener((e) -> {
+                 if (text.getText().equals(" ") || text.getText().equals("")){
+               
+                    Dialog.show("Ajout Publication"," champ est vide " ,"ok",null);
+                 
+             }
+             else{
             PublicationService ser = new PublicationService();
             Publication t = new Publication();
             t.setAdresse(adresse.getText());
@@ -227,6 +159,7 @@ AjouterB.setUIID("AjouterButton");
              fil.show();
             
             System.out.println(text.getText() + " " + adresse.getText()+""+fileNameInServer);
+                 }
         });
         this.addCommand(new Command("Fil Actualite") {
 
